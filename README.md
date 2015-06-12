381fafecd3f908a2dbf3749a23a45f482b5323bd
Joe Alba <joealba@JoeBookPro.local>
2015-06-12 16:35:52 -0400

    Saving google access token to a local gitignored file.  Still having problems where it quits after looping through a few dozen rows

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tinkering with Twitter and Google Spreadsheets to help make a makermap
+
+- https://github.com/sferik/twitter
+- https://github.com/gimite/google-drive-ruby
+
--- r.rb
+++ r.rb
@@ -2,7 +2,11 @@
 
 require 'bundler/setup'
 require 'yaml'
+
 require 'twitter'
+
+require "google/api_client"
+require "google_drive"
 
 module Makermap
 
@@ -10,19 +14,109 @@ module Makermap
     CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), 'creds.yml'))[:twitter].freeze
 
     def client
-      @client ||= Twitter::REST::Client.new CONFIG
+      @client ||= ::Twitter::REST::Client.new CONFIG
     end
 
-    def get_geo_info(tweet)
-      t = client.status tweet.to_i
-      t.geo
+    def get_tweet_by_id(tweet_id)
+      tweet = client.status tweet_id.to_i
+    end
+
+    def get_geo_info(tweet_id)
+      get_tweet_by_id(tweet_id).geo
     end
   end
 
   class Spreadsheet
     CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), 'creds.yml'))[:google].freeze
 
+    attr_reader :session
+
+    def initialize
+      access_token = get_local_access_token
+      access_token = setup_access_token if access_token == ''
+      @session = GoogleDrive.login_with_oauth(access_token)
+    end
+
+    def access_token_filename
+      File.join(File.dirname(__FILE__), '.access_token')
+    end
+
+    def get_local_access_token
+      begin
+        File.read(access_token_filename).chomp
+      rescue Errno::ENOENT
+        nil
+      end
+    end
+
+    def save_local_access_token(access_token)
+      File.open(access_token_filename, 'w') do |fh|
+        fh.write access_token
+      end
+    end
+
+    def setup_access_token
+      client = ::Google::APIClient.new
+      auth = client.authorization
+      auth.client_id = CONFIG[:client_id]
+      auth.client_secret = CONFIG[:client_secret]
+      auth.scope = [
+        "https://www.googleapis.com/auth/drive",
+        "https://spreadsheets.google.com/feeds/"
+      ]
+      auth.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+      print("1. Open this page:\n%s\n\n" % auth.authorization_uri)
+      print("2. Enter the authorization code shown in the page: ")
+      auth.code = $stdin.gets.chomp
+      auth.fetch_access_token!
+      access_token = auth.access_token
+
+      save_local_access_token access_token
+    end
+  end
+
+  class SpreadsheetUpdater
+    WORKSHEET_KEY = YAML.load_file(File.join(File.dirname(__FILE__), 'creds.yml'))[:google_spreadsheet][:document_key]
+
+    attr_reader :google_session, :ws, :twitter_client
+
+    def initialize
+      @google_session = Makermap::Spreadsheet.new.session
+      @twitter_client = Makermap::Twitter.new
+      open_google_doc
+    end
+
+    def open_google_doc
+      @ws = google_session.spreadsheet_by_key(WORKSHEET_KEY).worksheets[0]
+    end
+
+    alias_method :refresh_google_doc, :open_google_doc
+
+    def populate_geo_data(start = 1)
+      begin
+        (start.to_i..ws.num_rows).each do |row|
+          next if ws[row, 5] != '' ## Skip if geo already populated
+
+          tweet_id = ws[row, 3][/(\d+)\s*$/]
+          t = twitter_client.get_tweet_by_id tweet_id
+          next if t.retweet?  ## Skip if retweet -- not likely to have geo data?
+
+          geo_info = t.geo
+          puts "#{row}: #{ws[row, 3]}"
+          puts "  GEO: #{geo_info}"
+          ws[row, 5] = geo_info || 'nil'
+        end
+      rescue ::Twitter::Error::NotFound
+        ## The tweet is gone
+      end
+
+    end
+
   end
 end
 
+su = Makermap::SpreadsheetUpdater.new
+su.populate_geo_data
 
+## Different starting points
+# su.populate_geo_data 100
